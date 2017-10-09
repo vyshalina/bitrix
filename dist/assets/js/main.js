e7c5616654f69509d4f810c8631c652832e8848f
vyshalina <vyshalina@gmail.com>
2017-10-09 16:08:45 +0200

    responsive

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$('.js-burgerNav').click(function(e){
 		e.preventDefault();
 
+		$(body).toggleClass('ov-hidden');
 		$(this).toggleClass('active');
 		$('.js-nav').toggleClass('active');
 	});
