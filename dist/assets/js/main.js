540beb00ec124350705bcb0c2859d2ae553181a8
vyshalina <vyshalina@gmail.com>
2017-10-09 16:59:38 +0200

    gfdgd

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -7,3 +7,21 @@ $(document).ready(function() {
 		$('.js-nav').toggleClass('active');
 	});
 });
+
+$(function(){
+
+	$(".order").on("click", ".tabs a", function(){
+
+		var tabs = $(".order .tabs a"),
+		    cont = $(".order  .order-content article");
+
+		// Удаляем классы active
+		tabs.removeClass("active");
+		cont.removeClass("active");
+		// Добавляем классы active
+		$(this).addClass("active");
+		cont.eq($(this).index()).addClass("active");
+
+		return false;
+	});
+});
